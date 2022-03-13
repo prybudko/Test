9fbbf4ae7a3aad7fb0a08fab677b99b05ad65dfa
rprybudko <prybudko.roman@airslate.com>
2022-03-13 14:59:42 +0200

    first

---
+++ Test.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Models\Call;
+
+class Test
+{
+    public function awd()
+    {
+        echo "<pre>";
+        print_r(434);
+        echo "</pre>";
+        echo "23";
+    }
+
+    public function test()
+    {
+        $call = Call::filter($request->all())
+            ->with(["staff", "company", "status", "user", "internalUser"])
+            ->where("id", $id)
+            ->first();
+        $call = Call::filter($request->all())
+            ->with(["staff", "company", "status", "user", "internalUser"])
+            ->where("id", $id)
+            ->first(); // now will workwesf
+    }
+}
